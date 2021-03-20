eac0d97f5476ac41e3da2613e65bc4d346308497
Steven Retz <sretz@innovoft.com>
2021-03-20 14:20:09 -0300

    started class public sealed class SplitReader : IDisposable

---
+++ SplitReader.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Innovoft.IO
+{
+	public sealed class SplitReader : IDisposable
+	{
+		#region Fields
+		private Stream stream;
+		#endregion //Fields
+
+		#region Constructors
+		public SplitReader(Stream stream)
+		{
+			this.stream = stream;
+		}
+		#endregion //Constructors
+
+		#region Finalizer
+		~SplitReader()
+		{
+			Dispose(disposing: false);
+		}
+		#endregion //Finalizer
+
+		#region Methods
+		#region Dispose
+		public void Dispose()
+		{
+			Dispose(disposing: true);
+			GC.SuppressFinalize(this);
+		}
+
+		private void Dispose(bool disposing)
+		{
+			Interlocked.Exchange(ref stream, null)?.Dispose();
+		}
+		#endregion //Dispose
+		#endregion //Methods
+	}
+}
