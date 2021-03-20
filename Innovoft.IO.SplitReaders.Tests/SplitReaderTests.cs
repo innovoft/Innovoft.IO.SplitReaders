51edfedea16a81070c0cfe65e06fd8cc0697cf05
Steven Retz <sretz@innovoft.com>
2021-03-20 17:29:49 -0300

    implementing test ReadLineTest

--- Innovoft.IO.SplitReaders.Tests/SplitReaderTests.cs
+++ Innovoft.IO.SplitReaders.Tests/SplitReaderTests.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -8,8 +11,22 @@ namespace Innovoft.IO
 	public class SplitReaderTests
 	{
 		[TestMethod]
-		public void Test()
+		public void ReadLineTest()
 		{
+			const char separator = ',';
+			var expecteds = new string[] { "A", "B", "C", "D", };
+			var text = string.Join(separator.ToString(), expecteds);
+			var raw = Encoding.UTF8.GetBytes(text);
+			var actuals = new List<string>();
+			using (var reader = new SplitReader(null))
+			{
+				Assert.IsTrue(reader.ReadLine(separator, actuals), "!ReadLine");
+				Assert.AreEqual(expecteds.Length, actuals.Count, "expecteds.Length != actuals.Count");
+				for (var i = expecteds.Length - 1; i >= 0; --i)
+				{
+					Assert.AreEqual(expecteds[i], actuals[i], i.ToString());
+				}
+			}
 		}
 	}
 }
