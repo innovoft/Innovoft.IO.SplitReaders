32227512f81b0a41143037631361bf1e8019d281
Steven Retz <sretz@innovoft.com>
2021-03-20 17:12:25 -0300

    started tests Innovoft.IO.SplitReaders.Tests

---
+++ Innovoft.IO.SplitReaders.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Innovoft.IO.SplitReaders.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Innovoft")]
+[assembly: AssemblyProduct("Innovoft.IO.SplitReaders.Tests")]
+[assembly: AssemblyCopyright("Copyright Innovoft ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b6482819-a400-4ecb-9ebf-f140a87fb3d3")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
---
+++ Innovoft.IO.SplitReaders.Tests/SplitReaderTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Innovoft.IO
+{
+	[TestClass]
+	public class SplitReaderTests
+	{
+		[TestMethod]
+		public void Test()
+		{
+		}
+	}
+}
