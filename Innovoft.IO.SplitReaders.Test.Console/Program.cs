a89e44d5ad584ab80bde7dc8706c122c7fb43bd1
Steven Retz <sretz@innovoft.com>
2021-03-21 00:54:56 -0300

    started program Innovoft.IO.SplitReaders.Test.Console

---
+++ Innovoft.IO.SplitReaders.Test.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Innovoft.IO;
+
+namespace Innovoft.IO
+{
+	internal static class Program
+	{
+		#region Main
+		private static void Main(string[] args)
+		{
+		}
+		#endregion //Main
+
+		#region Fields
+		private static string soucePath;
+		#endregion //Fields
+
+		#region Methods
+		#endregion //Methods
+	}
+}
