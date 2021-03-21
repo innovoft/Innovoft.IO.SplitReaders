b09ac90ea980153cfe4545ba01835ae003379b25
Steven Retz <sretz@innovoft.com>
2021-03-21 01:01:42 -0300

    implemented flow

--- Innovoft.IO.SplitReaders.Test.Console/Program.cs
+++ Innovoft.IO.SplitReaders.Test.Console/Program.cs
@@ -12,14 +12,98 @@ namespace Innovoft.IO
 		#region Main
 		private static void Main(string[] args)
 		{
+			var errors = ParseCommandLine(args);
+			if (errors != null)
+			{
+				PrintHelp(errors);
+				return;
+			}
+
+			Prepare();
+			Results();
 		}
 		#endregion //Main
 
 		#region Fields
-		private static string soucePath;
+		private static string sourcePath;
+		private static char separator;
 		#endregion //Fields
 
 		#region Methods
+		private static List<string> ParseCommandLine(string[] args)
+		{
+			var errors = new List<string>();
+			if (args == null || args.Length == 0)
+			{
+				return errors;
+			}
+
+			for (var i = 0; i < args.Length; ++i)
+			{
+				switch (args[i])
+				{
+				default:
+					errors.Add("Invalid Parameter: " + args[i]);
+					break;
+
+				case "-Source":
+					try
+					{
+						sourcePath = args[++i];
+					}
+					catch (Exception exception)
+					{
+						errors.Add("Problems processing -Source " + exception.Message);
+					}
+					break;
+				}
+			}
+
+			if (sourcePath == null)
+			{
+				errors.Add("Missing -Source");
+			}
+
+			if (errors.Count > 0)
+			{
+				return errors;
+			}
+
+			return null;
+		}
+
+		private static void PrintHelp(List<string> errors)
+		{
+			Console.WriteLine();
+			Console.WriteLine("Innovoft.IO.SplitReaders.Test.Console.exe");
+			Console.WriteLine(" Required");
+			Console.WriteLine("  -Source Source.csv");
+			Console.WriteLine(" Optional");
+			Console.WriteLine("  -Separator ,");
+			Console.WriteLine();
+
+			if (errors != null && errors.Count > 0)
+			{
+				foreach (var error in errors)
+				{
+					Console.WriteLine(error);
+				}
+				Console.WriteLine();
+				Console.WriteLine(Environment.CommandLine);
+				Console.WriteLine();
+			}
+		}
+
+		private static void Prepare()
+		{
+			Console.Title = "Innovoft.IO.SplitReaders.Test.Console";
+
+			GC.Collect();
+		}
+
+		private static void Results()
+		{
+		}
 		#endregion //Methods
 	}
 }
