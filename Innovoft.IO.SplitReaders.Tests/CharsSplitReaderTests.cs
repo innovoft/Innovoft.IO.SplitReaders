3a9a0bc716591d9b2d17ea7f9746da799f620517
Steven Retz <sretz@innovoft.com>
2021-03-25 14:28:59 -0300

    added test ReadColumnTest

--- Innovoft.IO.SplitReaders.Tests/CharsSplitReaderTests.cs
+++ Innovoft.IO.SplitReaders.Tests/CharsSplitReaderTests.cs
@@ -30,5 +30,24 @@ namespace Innovoft.IO
 				}
 			}
 		}
+
+		[TestMethod]
+		public void ReadColumnTest()
+		{
+			const char separator = ',';
+			var expecteds = new string[] { "A", "B", "C", "D", "", };
+			var text = string.Join(separator.ToString(), expecteds);
+			var raw = Encoding.UTF8.GetBytes(text);
+			for (var i = expecteds.Length - 1; i >= 0; --i)
+			{
+				var actual = new CharsColumn();
+				using (var readerStream = new MemoryStream(raw))
+				using (var reader = new CharsSplitReader(readerStream))
+				{
+					Assert.IsTrue(reader.ReadColumnsLength(separator, i, actual.AppendLength), "!ReadColumn");
+					Assert.AreEqual(expecteds[i], actual.ToString(), i.ToString());
+				}
+			}
+		}
 	}
 }
