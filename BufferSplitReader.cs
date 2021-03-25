070474e84cf28d6bfadf4c235eaccb0934e9f2f0
Steven Retz <sretz@innovoft.com>
2021-03-25 02:54:06 -0300

    started class public sealed partial class BufferSplitReader : SplitReader

---
+++ BufferSplitReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Innovoft.IO
+{
+	public sealed partial class BufferSplitReader : SplitReader
+	{
+		#region Constructors
+		public BufferSplitReader(Stream stream)
+			: base(stream)
+		{
+		}
+		#endregion //Constructors
+	}
+}
