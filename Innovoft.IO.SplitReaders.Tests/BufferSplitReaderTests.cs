2cecf4acaf82bfdd8545c308dd031aa1a0a73d81
Steven Retz <sretz@innovoft.com>
2021-03-25 12:01:26 -0300

    added test ReadColumnsAddTest

---
+++ Innovoft.IO.SplitReaders.Tests/BufferSplitReaderTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Innovoft.IO
+{
+	[TestClass]
+	public class BufferSplitReaderTests
+	{
+		[TestMethod]
+		public void ReadColumnsAddTest()
+		{
+			const byte separator = BufferSplitReader.Comma;
+			var expecteds = new string[] { "A", "B", "C", "D", "", };
+			var text = string.Join(((char)separator).ToString(), expecteds);
+			var raw = Encoding.UTF8.GetBytes(text);
+			var actuals = new BufferColumns();
+			using (var readerStream = new MemoryStream(raw))
+			using (var reader = new BufferSplitReader(readerStream))
+			{
+				Assert.IsTrue(reader.ReadColumnsLength(separator, actuals.AddLength), "!ReadColumns");
+				Assert.AreEqual(expecteds.Length, actuals.Count, "expecteds.Length != actuals.Count");
+				var actualsColumns = actuals.ToArray();
+				for (var i = expecteds.Length - 1; i >= 0; --i)
+				{
+					Assert.AreEqual(expecteds[i], actualsColumns[i], i.ToString());
+				}
+			}
+		}
+	}
+}
