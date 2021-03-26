11773ce7fdfd027b07766af0f65497cc86d353c8
Steven Retz <sretz@innovoft.com>
2021-03-25 21:23:41 -0300

    added test BufferSplitReaderTests.ReadColumnsSetTest

--- Innovoft.IO.SplitReaders.Tests/BufferSplitReaderTests.cs
+++ Innovoft.IO.SplitReaders.Tests/BufferSplitReaderTests.cs
@@ -49,5 +49,36 @@ namespace Innovoft.IO
 				}
 			}
 		}
+
+		[TestMethod]
+		public void ReadColumnsSetTest()
+		{
+			const byte separator = BufferSplitReader.Comma;
+			var expecteds = new string[] { "A", "B", "C", "D", "", };
+			var text = string.Join(((char)separator).ToString(), expecteds);
+			var raw = Encoding.UTF8.GetBytes(text);
+			var columns = new int[expecteds.Length];
+			for (var column = columns.Length - 1; column >= 0; --column)
+			{
+				columns[column] = column;
+			}
+			var actuals = new BufferColumn[expecteds.Length];
+			var appends = new Action<byte[], int, int>[expecteds.Length];
+			for (var i = actuals.Length - 1; i >= 0; --i)
+			{
+				var actual = new BufferColumn(Encoding.UTF8);
+				actuals[i] = actual;
+				appends[i] = actual.AppendLength;
+			}
+			using (var readerStream = new MemoryStream(raw))
+			using (var reader = new BufferSplitReader(readerStream))
+			{
+				Assert.IsTrue(reader.ReadColumnsLength(separator, appends), "!ReadColumns");
+				for (var i = expecteds.Length - 1; i >= 0; --i)
+				{
+					Assert.AreEqual(expecteds[i], actuals[i].ToString(), i.ToString());
+				}
+			}
+		}
 	}
 }
