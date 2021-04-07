b2e6b0aa9bb41d73ecb9d830d1b3b5856113ee2a
Steven Retz <sretz@innovoft.com>
2021-04-06 23:23:18 -0300

    added method public void AppendLength(byte[] append, int offset, int length, Encoding encoding)

--- CharsColumn.cs
+++ CharsColumn.cs
@@ -146,6 +146,23 @@ namespace Innovoft.IO
 			count += length;
 		}
 
+		public void AppendLength(byte[] append, int offset, int length, Encoding encoding)
+		{
+			appended = true;
+			var decodedLength = encoding.GetCharCount(append, offset, length);
+			var required = count + decodedLength;
+			if (required > capacity)
+			{
+				var enlargedCapacity = 2 * capacity;
+				var enlarged = new char[enlargedCapacity];
+				Array.Copy(letters, 0, enlarged, 0, count);
+				capacity = enlargedCapacity;
+				letters = enlarged;
+			}
+			encoding.GetChars(append, offset, length, letters, count);
+			count += decodedLength;
+		}
+
 		public void AppendLength(char[] append, int offset, int length)
 		{
 			appended = true;
