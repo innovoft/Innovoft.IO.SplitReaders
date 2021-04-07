8cf03382dfa1c11dbfb40e8e939cfd8e1894086f
Steven Retz <sretz@innovoft.com>
2021-04-06 22:04:17 -0300

    added method public void AppendEnding(string append, int offset, int ending)

--- CharsColumn.cs
+++ CharsColumn.cs
@@ -107,6 +107,23 @@ namespace Innovoft.IO
 			count += length;
 		}
 
+		public void AppendEnding(string append, int offset, int ending)
+		{
+			appended = true;
+			var length = ending - offset;
+			var required = count + length;
+			if (required > capacity)
+			{
+				var enlargedCapacity = 2 * capacity;
+				var enlarged = new char[enlargedCapacity];
+				Array.Copy(letters, 0, enlarged, 0, count);
+				capacity = enlargedCapacity;
+				letters = enlarged;
+			}
+			append.CopyTo(offset, letters, count, length);
+			count += length;
+		}
+
 		public void AppendLength(char[] append, int offset, int length)
 		{
 			appended = true;
