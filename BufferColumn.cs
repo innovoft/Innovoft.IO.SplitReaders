a3777773588f5aa8139b7cef60ee4f573055de6b
Steven Retz <sretz@innovoft.com>
2021-04-14 22:10:57 -0300

    added operator public static implicit operator ReadOnlySpan<byte>(BufferColumn value)

--- BufferColumn.cs
+++ BufferColumn.cs
@@ -245,6 +245,11 @@ namespace Innovoft.IO
 		{
 			return new ReadOnlySpan<byte>(buffer, 0, count);
 		}
+
+		public static implicit operator ReadOnlySpan<byte>(BufferColumn value)
+		{
+			return value.ToRead();
+		}
 #endif //NETSTANDARD2_1 || NET5_0_OR_GREATER
 
 #if NETSTANDARD2_1 || NET5_0_OR_GREATER
