2432d948f1b08525c6962db00412f78500a4488e
Steven Retz <sretz@innovoft.com>
2021-11-02 23:52:09 -0300

    [MethodImpl(MethodImplOptions.AggressiveInlining)]

--- StringSplitReader.cs
+++ StringSplitReader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Innovoft.IO
@@ -20,6 +21,7 @@ namespace Innovoft.IO
 		#endregion //Constructors
 
 		#region Methods
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		private new void OpenOnly(Stream stream, bool dispose)
 		{
 			base.OpenOnly(stream, dispose);
