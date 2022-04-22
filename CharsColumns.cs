2c99f83e7303b7f5c9e2a751a57038ca3304c102
Steven Retz <sretz@innovoft.com>
2022-04-22 00:57:09 -0300

    added method public static void Clear(CharsColumns value)

--- CharsColumns.cs
+++ CharsColumns.cs
@@ -12,6 +12,14 @@ namespace Innovoft.IO
 		public const int DefaultCapacity = CharsColumn.DefaultCapacity;
 		#endregion //Constants
 
+		#region Class Methods
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static void Clear(CharsColumns value)
+		{
+			value.Clear();
+		}
+		#endregion //Class Methods
+
 		#region Fields
 		private int capacity;
 		#endregion //Fields
